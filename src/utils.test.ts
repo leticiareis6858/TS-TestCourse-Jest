b63b80bc92ce164354d3d3fc0208986166453c35
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2023-09-16 23:14:21 -0300

    trying nodeJs test runner module

---
+++ src/utils.test.ts
@@ -0,0 +1,23 @@
+import { test, describe, mock } from "node:test";
+import * as assert from "node:assert";
+import { strictEqual } from "node:assert";
+import { toUpperCase } from "./utils";
+
+describe("node tests trials", () => {
+  test("toUpperCase", () => {
+    const actual = toUpperCase("abc");
+    const expected = "ABC";
+    assert.strictEqual(actual, expected);
+    //strictEqual(actual, expected);
+  });
+
+  test("sum mock", () => {
+    const toUpperCaseMock = mock.fn((arg) => {
+      return toUpperCase(arg);
+    });
+
+    assert.strictEqual(toUpperCaseMock.mock.calls.length, 0);
+    toUpperCaseMock("abc");
+    assert.strictEqual(toUpperCaseMock.mock.calls.length, 1);
+  });
+});
